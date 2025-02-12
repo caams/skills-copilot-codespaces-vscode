679644dfd0b2cdf6c2d5cbb5403814cdaa791b2b
caams <41647243+caams@users.noreply.github.com>
2025-02-12 03:48:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,55 @@
+// Create web server
+// --------------------------
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsFile = path.join(__dirname, 'comments.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up the server
+app.set('port', (process.env.PORT || 3000));
+
+// Set up the server
+app.set('port', (process.env.PORT || 3000));
+
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Get the comments
+app.get('/api/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+// Add a comment
+app.post('/api/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+            id: Date.now(),
+            author: req.body.author,
+            text: req.body.text
+          };
+          comments.push(newComment);
+          fs.writeFile(commentsFile, JSON.stringify(comments, null, 4), function(err) {
+            if (err) {
+              console.error(err);
+              process.exit(1);
+            }
+            res.json(comments);
+          });
+        });
+      });
